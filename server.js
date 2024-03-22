81087c285657c9e06df23571156e043807097fa1
Souleima18 <126724135+Souleima18@users.noreply.github.com>
2024-03-22 12:15:24 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ const app = express();
 const categorieRoute=require('./routes/categorie');
 const produitRoute=require('./routes/produit');
 const userRoute=require('./routes/user');
-
+const favoriteRoute=require('./routes/favorite');
 
 app.use(express.json());
 
 app.use('/categorie',categorieRoute);
 app.use('/produit',produitRoute);
 app.use('/user',userRoute);
+app.use('/favorite',favoriteRoute);
 app.use(express.json());
 
 app.use('/image' , express.static('./uploads'));

593bc5c627f1dc16d0757c3f567b1fe4b5e2b6ed
ser.name Your <djebbihaitem9@gmail.com>
2024-05-09 17:20:17 +0100

    pfa

--- models/commandecouleur.js
+++ models/commandecouleur.js
@@ -15,7 +15,22 @@ const CommandeCouleurSchema = new Schema({
     quantite: {
         type: Number,
         default: 1 
-    }   
+    } , 
+    prix : {
+        type : Number,
+    }  ,
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    livree : {
+        type: Boolean,
+        default : false
+    },
+    dateLivraison: {
+        type: Date, // Champ pour stocker la date de livraison
+        default: null // La date de livraison est nulle par défaut, car la commande n'a pas été livrée
+    }
 });
 
 module.exports = mongoose.model('CommandeCouleur', CommandeCouleurSchema);
